06d4bb1108c584bb63b97fb68247fa702c932da4
menegasse <menegassehugo12@gmail.com>
2022-06-08 10:37:57 -0300

    porject start

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link"
+
+export default function Page404(){
+    return (
+        <div>
+            <h1>Alura Cases - Página não encontrada (404)</h1>
+            <Link href="/">
+                Ir para página Home.
+            </Link>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+// import Link from "../src/components/Link"
+// import Footer from "../src/components/patterns/Footer";
+
+// function Title({children, as}){
+//     const Tag = as;
+
+//     return (
+//         <>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag}{
+//                     color: red;
+//                 }
+//             `}</style>
+//         </>
+//     );
+// }
+
+// export default function HomePage(){
+//     return (
+//         <div>
+//             <Title as="h1">Alura Cases - Home Page</Title>
+//             <Link href="/faq">
+//                 Ir para página de FAQ.
+//             </Link>
+//             <Footer/>
+//         </div>
+//     )
+// }
